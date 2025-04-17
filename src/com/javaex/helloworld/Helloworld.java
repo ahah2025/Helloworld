fb85d27a7c467f8f06c346f448a71f5a951befa6
ahah2025 <adghty61>
2025-04-17 09:49:27 +0900

    1st

---
+++ src/com/javaex/helloworld/Helloworld.java
@@ -0,0 +1,17 @@
+/*
+1.작성자:이아름
+2.작성일:2025.04.14
+3.설 명 : Hello World 출력하기
+*/
+
+package com.javaex.helloworld;
+
+public class Helloworld {
+	
+	//문자열을 화면에 출력하는 함수:println
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+	
+}
